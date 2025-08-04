91b157cd90b2a27e1c6d0700101707c547de0b66
w3schools-test <test@w3schools.com>
2025-08-04 14:46:52 +0000

    "Inheritance And Method Overriding"

---
+++ InheritanceAndMethodOverriding/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        animal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
