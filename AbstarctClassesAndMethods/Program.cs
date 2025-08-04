695303b942606b37c2c4dac033f9e67b14d33db1
w3schools-test <test@w3schools.com>
2025-08-04 14:57:51 +0000

    "Abstarct Classes And Methods"

---
+++ AbstarctClassesAndMethods/Program.cs
@@ -0,0 +1,48 @@
+﻿public abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape circle = new Circle(7);
+        Shape rectangle = new Rectangle(6, 9);
+
+        Console.WriteLine("Circle Area: " + circle.GetArea());
+        Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
+    }
+}
